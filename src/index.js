0d58b3a81f335a67a00a556ac1e7c071728a7de0
unknown <lily@l1lith.com>
2019-10-15 10:26:14 -0600

    make command input more intelligent

--- src/index.js
+++ src/index.js
@@ -18,10 +18,20 @@ const args = arg({
 const primaryOptionNames = ["--configure", "--generate", "--list"]
 
 const primaryOptions = Object.entries(args).filter(([name, value]) => primaryOptionNames.includes(name) && value === true)
+
+if (primaryOptions.length < 1 && primaryOptionNames.includes('--' + args._[0])) {
+  const command = '--'+args._[0]
+  args._ = args[command] = args._.slice(1)
+  primaryOptions.push([command])
+
+}
+
 if (primaryOptions.length > 1) throw new Error("Too Many Primary Options")
+
 const primaryOption = (primaryOptions[0] || [])[0] || "--generate"
 const commandFunction = require("./commands/" + primaryOption.substring(2))
 const result = commandFunction(args)
+
 if (result instanceof Promise) {
   result.then(()=>{
     //process.exit(0)
