a65a179fb6db36c50ee33847de825dfde64743ea
unknown <lily@l1lith.com>
2019-10-15 10:44:55 -0600

    improve the help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,5 +1,5 @@
 function help() {
-  console.log(" Please refer to the Readme at\n- https://github.com/L1lith/Emerald-Templates")
+  console.log(" Available Commands:\n- generate, list, configure\nFor more information, please refer to the Readme at\n- https://github.com/L1lith/Emerald-Templates")
 }
 
 module.exports = help
