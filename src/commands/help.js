84af997b10ee3c26a66c7492cfe01edd28cabe9d
BuildTools <lily@l1lith.com>
2020-10-08 02:51:19 -0600

    add more help for all the commands

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,8 +3,17 @@ const commands = ["generate", "configure", "list", 'add-root', 'add-template',"d
 
 const commandHelp = {
   generate: chalk.green("Function: ") + "Generates a new project from a template" + chalk.green("\nSyntax: ") + chalk.cyan("generate <template name> <project name>"),
+  describe: chalk.green("Function: ") + "Displays information about a template" + chalk.green("\nSyntax: ") + chalk.cyan("describe <template name>"),
+  "get-directory": chalk.green("Function: ") + "Prints out the path to a given template" + chalk.green("\nSyntax: ") + chalk.cyan("get-directory <template name>"),
+  "add-root": chalk.green("Function: ") + "Adds a new root folder given a relative or absolute path" + chalk.green("\nSyntax: ") + chalk.cyan("add-root <root path>"),
+  "remove-root": chalk.green("Function: ") + "Removes an existing root folder given a relative or absolute path" + chalk.green("\nSyntax: ") + chalk.cyan("remove-root <root path>"),
+  "add-template": chalk.green("Function: ") + "Adds a new template folder given a relative or absolute path" + chalk.green("\nSyntax: ") + chalk.cyan("add-template <template path>"),
+  "remove-template": chalk.green("Function: ") + "Removes an existing template folder given a relative or absolute path" + chalk.green("\nSyntax: ") + chalk.cyan("remove-template <template path>"),
   configure: chalk.green("Function: ") + "Configures emerald templates, no args necessary" + chalk.green("\nSyntax: ") + chalk.cyan("configure"),
   list: chalk.green("Function: ") + "Lists all the available templates, no args necessary" + chalk.green("\nSyntax: ") + chalk.cyan("list"),
+  "list-roots": chalk.green("Function: ") + "Lists all the root template folders, no args necessary" + chalk.green("\nSyntax: ") + chalk.cyan("list-roots"),
+  version: chalk.green("Function: ") + "Displays the current version of Emerald Templates, no args necessary" + chalk.green("\nSyntax: ") + chalk.cyan("version"),
+  open: chalk.green("Function: ") + "Opens a given template with your file browser" + chalk.green("\nSyntax: ") + chalk.cyan("open <template name>"),
   help: chalk.green("Function: ") + "Provides help about how to use emerald templates. Either provide no args for general information, or specify a command to learn more about it" + chalk.green("\nSyntax: ") + chalk.cyan("help {command}")
 }
 
@@ -20,9 +29,10 @@ function help(options) {
     }
     return
   } else if (commandRequest !== true) {
+    console.log(options)
     throw new Error("Invalid Help Value, please either leave it blank or specify a command you'd like to know more about")
   }
-  console.log(` Available Commands: \n- ${commands.map(command => chalk.green(command)).join(', ')}\n\nFor more information, please refer to the Readme at\n- ${chalk.bold(chalk.green("https://github.com/L1lith/Emerald-Templates"))}`)
+  console.log(`To get help about a specific command try this:\n  ${chalk.cyan("emt -h=<command>")}\n\nAvailable Commands: \n- ${commands.map(command => chalk.green(command)).join(', ')}\n\nFor more information, please refer to the Readme at\n- ${chalk.bold(chalk.green("https://github.com/L1lith/Emerald-Templates"))}`)
 }
 
 module.exports = help
--- src/commands/list-roots.js
+++ src/commands/list-roots.js
@@ -6,7 +6,7 @@ function listRoots() {
   if (rootFolders.length < 1) {
     throw new Error("There are no root folders")
   }
-  console.log(` Configured for the following root folders:\n- ${rootFolders.map(value => chalk.green(value)).join(', ')}`)
+  console.log(` Configured for the following root folders:\n${rootFolders.map(value => '- ' + chalk.green('"' + value + '"')).join('\n')}`)
 }
 
 module.exports = listRoots
