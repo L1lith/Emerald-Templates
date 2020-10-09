aedba595330e5452492d3da491c0a47eba8ea7c4
BuildTools <lily@l1lith.com>
2020-10-09 17:36:07 -0600

    change version

--- src/commands/version.js
+++ src/commands/version.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 const {version} = require('../../package.json')
 
 function printVersion() {
-  console.log(chalk.green("Emerald Templates " + chalk.bold("v" + version) + " by L1lith"))
+  console.log(chalk.green("Emerald Templates " + chalk.bold("v" + version)) + chalk.cyan(" by L1lith"))
 }
 
 module.exports = printVersion
