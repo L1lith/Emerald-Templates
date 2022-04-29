04e60ff80d2c8e7d84c90c3ac22bb5f753fbc50f
L1lith <lily@l1lith.com>
2022-04-29 12:46:15 -0600

    try to fix the stuff

--- test/createProject/expectedOutputs/test-1/no-input/rollup.config.js
+++ test/createProject/expectedOutputs/test-1/no-input/rollup.config.js
@@ -6,50 +6,49 @@ import { uglify } from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
 import deepmerge from 'deepmerge'
 
-
-const name = "no-input"
-const target = 
+const name = 'no-input'
+//const target =
 
 const baseConfig = {
-    input: join(__dirname, 'source', 'index.js'),
-    output: {
-        name
-    },
-    name,
-    plugins: [
-        resolve({ jsnext: true }),
-        commonjs({
-            include: 'node_modules/**'
-        }),
-        babel({
-            exclude: 'node_modules/**',
-            presets: ['@babel/preset-env']
-        }),
-        uglify({
-            sourcemap: false
-        })
-    ]
+  input: join(__dirname, 'source', 'index.js'),
+  output: {
+    name
+  },
+  name,
+  plugins: [
+    resolve({ jsnext: true }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env']
+    }),
+    uglify({
+      sourcemap: false
+    })
+  ]
 }
 
 const branchConfigs = {
-    browser: {
-        output: {
-            format: 'iife',
-            file: join(__dirname, 'dist', 'no-input-browser.min.js')
-        }
-    },
-    node: {
-        output: {
-            format: 'cjs',
-            file: join(__dirname, 'dist', 'no-input-commonjs.js')
-        }
-    },
-    universal: {
-        output: {
-            format: 'umd',
-            file: join(__dirname, 'dist', 'no-input-universal.min.js')
-        }
+  browser: {
+    output: {
+      format: 'iife',
+      file: join(__dirname, 'dist', 'no-input-browser.min.js')
     }
+  },
+  node: {
+    output: {
+      format: 'cjs',
+      file: join(__dirname, 'dist', 'no-input-commonjs.js')
+    }
+  },
+  universal: {
+    output: {
+      format: 'umd',
+      file: join(__dirname, 'dist', 'no-input-universal.min.js')
+    }
+  }
 }
 
 const configs = branchConfigs.map(config => deepmerge(baseConfig, config))
